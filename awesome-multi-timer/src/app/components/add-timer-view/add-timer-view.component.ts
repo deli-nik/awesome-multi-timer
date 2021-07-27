3480a2a09fd416c041a79fe3f9aad06ee6e966c0
Nik Adli Hakimi <adli89@gmail.com>
2021-07-27 23:10:52 +0800

    Can modify time left now!
    Co-authored-by: anas-mohd-nazlee <anas-mohd-nazlee@users.noreply.github.com>

--- awesome-multi-timer/src/app/components/add-timer-view/add-timer-view.component.ts
+++ awesome-multi-timer/src/app/components/add-timer-view/add-timer-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input, OnDestroy } from '@angular/core';
 import { Observable, interval } from 'rxjs';
-import { map, take, takeUntil, takeWhile } from 'rxjs/operators';
+import { delay, map, mapTo, take, takeUntil, takeWhile, tap } from 'rxjs/operators';
 import { ConvertMsToTimeDisplay, ConvertTimeDisplayToMs } from '../../functions/time-util';
 import { TimeDisplay } from '../../models/time-display';
 import { TimerConfig } from '../../models/timer-config';
@@ -17,21 +17,33 @@ export class AddTimerViewComponent implements OnInit {
   timerConfig!: TimerConfig;
 
   totalTimeLeftInMs!: Observable<number>;
+  counter!: number;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.totalTimeLeftInMs = this.getTimerObservable();
+
+    setTimeout(() => this.counter += 3000, 2000);
   }
 
   getTimerObservable(): Observable<number>{
-    const initialTime = ConvertTimeDisplayToMs(this.timerConfig.countdownTime);
+    const initialTime = 10000;//ConvertTimeDisplayToMs(this.timerConfig.countdownTime);
     const intervalInMs = this.timerConfig.countdownIntervalInMs;
 
+    this.counter = initialTime;
+    // https://stackoverflow.com/questions/35419062/how-to-stop-and-resume-observable-interval-emiting-ticks
+
     return interval(this.timerConfig.countdownIntervalInMs)
-            .pipe(
-              takeWhile(seq => seq * intervalInMs != initialTime, true),
-              map(seq => initialTime - (seq * intervalInMs)));
+             .pipe(
+               takeWhile(seq => this.counter > 0),
+               tap(seq =>
+                {
+                  this.counter = this.counter > intervalInMs ?
+                    this.counter - intervalInMs : 0;
+                }),
+               map(seq => this.counter)
+             );
   }
 }
